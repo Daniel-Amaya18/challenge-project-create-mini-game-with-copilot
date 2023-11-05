a6c63c7f64f66435637733f522b85194cbad7247
Daniel-Amaya18 <62664190+Daniel-Amaya18@users.noreply.github.com>
2023-11-05 23:32:43 +0000

    Versión final juego de piedras, papel o tijeras

--- app.py
+++ app.py
@@ -3,9 +3,42 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
+# Select a random element from a list
+from random import choice
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
+
+puntuacion = 0
+jugar = True
+opciones = ["piedra", "papel", "tijeras"]
+while jugar:
+    valido = False
+    while not valido:
+        jugador = input("Piedra, papel o tijeras: ").lower()
+        if jugador not in opciones:
+            print("Opción inválida")
+            continue
+        valido = True
+    computadora = choice(opciones)
+    if jugador == computadora:
+        print("Empate")
+    elif jugador == "piedra" and computadora == "tijeras":
+        print("Ganaste")
+        puntuacion += 1
+    elif jugador == "papel" and computadora == "piedra":
+        print("Ganaste")
+        puntuacion += 1
+    elif jugador == "tijeras" and computadora == "papel":
+        print("Ganaste")
+        puntuacion += 1
+    else:
+        print("Perdiste")
+    again = input("¿Jugar de nuevo? (s/n): ").lower()
+    if again == "n":
+        jugar = False
+        print("Tu puntuación final es: " + str(puntuacion) + " puntos")
+
